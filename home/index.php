678cb7504b3bd692c01de7854773206464fb6847
modkhalid <modkhalid5@gmail.com>
2018-10-14 16:47:11 +0530

    settings updated

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
 <head>
   <title>Code Chain | Home</title>
  <?php include '../common/common-code.php'; topHeader(); ?>
-
+<link rel="icon" type="image/gif" href="../img/logo.png">
   <style type="text/css">
   .mod-margin{
     margin-top: 50px;
@@ -49,39 +49,7 @@
 <body>
 <!-- nav here -->
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-  <a class="navbar-brand" href="index.php"><img src="../img/logo.png" width="30" height="30"> Code Chain</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav container justify-content-end">
-      <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../img/logo.png" width="27" height="27" style="border-radius: 50%;"> Mohammad Anas</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><i class="fas fa-user-circle"></i> Profile</a>
-           <a class="dropdown-item" href="#"><i class="fas fa-envelope"></i> Messages</a>
-          <a class="dropdown-item" href="#"><i class="fas fa-sliders-h"></i> Settings</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Log Out</a>
-        </div>
-      </li>
-
-       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fas fa-globe"></i> About</a>
-      </li>
-
-       <li class="nav-item dropdown ">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-bell"></i> Bells</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Bell 1</a>
-          <a class="dropdown-item" href="#">Bell 2</a>
-          <a class="dropdown-item" href="#">Bell 3</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<?php nav();?>
   
 
 <!-- nav ends here -->
---
+++ settings/profile/code/inc.profile.php
@@ -0,0 +1,51 @@
+<?php
+	function topitem(){
+		?>
+		<div class="row">
+			<div class="col-md-2">
+				
+			</div>
+			<div class="col-md-8" style="text-align: center;margin-top: 15px;">
+				<!-- <h1>hello mod khaud</h1> -->
+				<div class="w3-card-2 btn-group">
+					<button type="button" class="btn btn-white">Apple</button>
+					  <button type="button" class="btn btn-white">Samsung</button>
+					  <button type="button" class="btn btn-white">Sony</button>
+					  <button type="button" class="btn btn-white">Samsung</button>
+					  <button type="button" class="btn btn-white">Sony</button>
+					  <button type="button" class="btn btn-white">Samsung</button>
+					  <button type="button" class="btn btn-white">Sony</button>
+					  <button type="button" class="btn btn-white">Samsung</button>
+					  <button type="button" class="btn btn-white">Sony</button>
+
+				</div>
+			</div>
+			<div class="col-md-2">
+				
+			</div>
+
+		</div>
+
+
+		<?php
+	}
+
+
+	function editbio(){
+		?>
+		<div class="row">
+			<div class="col-8">
+				
+			</div>
+			<div class="col-4">
+				
+			</div>
+		</div>
+
+
+
+		<?php
+	}
+
+
+?>
