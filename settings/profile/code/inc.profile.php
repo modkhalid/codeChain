d4055d33644b618620ce39fd09fc01baef28bb54
modkhalid <modkhalid5@gmail.com>
2018-10-15 16:32:19 +0530

    profilepage updated

--- settings/profile/code/inc.profile.php
+++ settings/profile/code/inc.profile.php
@@ -33,12 +33,116 @@
 
 	function editbio(){
 		?>
+		<h3 class=" w3-border-bottom" style="margin-top: 20px;">Public profile</h3>
+			<!-- <hr > -->
 		<div class="row">
-			<div class="col-8">
+
+			<div class="col-7 w3-margin-top">
 				
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>Name<span class="text-danger">*</span></b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+
+
+				<div style="line-height: .7;">
+					<p><b>Public email</b></p>
+					<div class="input-group mb-3">
+					  
+					  <input type="text" class="form-control" style="width: 50%;">
+					</div>
+					
+
+				</div>
+
+
+				<div style="line-height: .7;">
+					<p><b>Bio</b></p>
+					<div class="input-group mb-3">
+					  
+					  <textarea type="text" class="form-control" style="width: 50%;"></textarea>
+					</div>
+					
+
+				</div>
+				<div style="line-height: .7;">
+					<p><b>Date of Birth</b></p>
+					<div class="input-group mb-3">
+					  
+					  <input type="date" class="form-control" >
+					</div>
+					
+
+				</div>
+
+
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>Phone Number</b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+
+
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>City</b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>Country</b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>Institute</b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+				<div style="line-height: .7;" class="w3-margin-top">
+					<p><b>Designation</b></p>
+					<div class="input-group mb-3">
+					  <input type="text" class="form-control">
+					</div>
+					
+
+				</div>
+				
+
+
+
+
+
+
+
+
+
+
 			</div>
-			<div class="col-4">
+			<div class="col-1"></div>
+			<div class="col-4 w3-margin-top">
 				
+					<img src="../../img/anas.jpeg" class="mx-auto d-block w3-card-4" style="width: 50%;border-radius: 50%;">
+					<button class="btn btn-outline-primary w3-margin-top mx-auto d-block" style="width: 50%;">Upload image</button>
+
+
+
+
 			</div>
 		</div>
 
