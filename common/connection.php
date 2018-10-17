c59c797fe57e39049b7e866a3d0e7d0c68d5998b
modkhalid <modkhalid5@gmail.com>
2018-10-17 11:17:40 +0530

    get or set method updated for userid

--- common/connection.php
+++ common/connection.php
@@ -9,4 +9,25 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
 
+
+
+function getId($session){
+	$sender=$session;
+	$sender=hex2bin($sender);
+	$sender=base64_decode($sender);
+	$sender=(($sender/5)-300)/5;
+	return sender;
+}
+
+
+
+function setId($value){
+	$user_id=$value;
+	$user_id=($user_id*5+300)*5;
+	$user_id=base64_encode($user_id);
+	$user_id=bin2hex($user_id);
+	return $user_id;
+}
+
+
 ?>
